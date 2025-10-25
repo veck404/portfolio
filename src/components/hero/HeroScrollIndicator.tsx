c5e86e043a00d0ce58c932a94b35687e0f042ccb
Veck404 <victor7ultimate@gmail.com>
2025-10-25 23:05:38 +0100

    lex

--- src/components/hero/HeroScrollIndicator.tsx
+++ src/components/hero/HeroScrollIndicator.tsx
@@ -1,16 +1,16 @@
-import { ArrowDown } from "lucide-react";
-import { Link } from "../Link";
+// import { ArrowDown } from "lucide-react";
+// import { Link } from "../Link";
 
-export function HeroScrollIndicator() {
-  return (
-    <div className="absolute bottom-[-1rem] left-1/2 -translate-x-1/2 animate-bounce">
-      <Link
-        href="#about"
-        // aria-label="Scroll to About section"
-        className="p-2 rounded-full bg-white/10 dark:bg-gray-800/10 hover:bg-white/20 dark:hover:bg-gray-800/20 transition-colors"
-      >
-        <ArrowDown className="w-6 h-6 text-gray-600 dark:text-gray-300" />
-      </Link>
-    </div>
-  );
-}
+// export function HeroScrollIndicator() {
+//   return (
+//     <div className="absolute bottom-[-1rem] left-1/2 -translate-x-1/2 animate-bounce">
+//       <Link
+//         href="#about"
+//         aria-label="Scroll to About section"
+//         className="p-2 rounded-full bg-white/10 dark:bg-gray-800/10 hover:bg-white/20 dark:hover:bg-gray-800/20 transition-colors"
+//       >
+//         <ArrowDown className="w-6 h-6 text-gray-600 dark:text-gray-300" />
+//       </Link>
+//     </div>
+//   );
+// }
